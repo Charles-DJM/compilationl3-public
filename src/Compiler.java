3da7dc8521cc78dacc3e8da7e8c84f240208587b
Bart-soldier <38186911+Bart-soldier@users.noreply.github.com>
2020-03-03 18:14:01 +0100

    TP3 finit.

--- src/Compiler.java
+++ src/Compiler.java
@@ -3,6 +3,7 @@ import sc.lexer.*;
 import sc.node.*;
 import java.io.*;
 import sa.*;
+import ts.Ts;
 //import ts.*;
 //import c3a.*;
 //import nasm.*;
@@ -46,11 +47,11 @@ public class Compiler
 	    SaNode saRoot = sc2sa.getRoot();
 	    new Sa2Xml(saRoot, baseName);
 
-	    /*
 	    System.out.println("[TABLE SYMBOLES]");
-	    Ts table = new Sa2ts(saRoot).getTableGlobale();
+	    Ts table = new Sa2ts(saRoot).getTable();
 	    table.afficheTout(baseName);
 
+		/*
 	    System.out.println("[C3A]");
 	    C3a c3a = new Sa2c3a(saRoot, table).getC3a();
 	    c3a.affiche(baseName);
---
+++ src/Sa2ts.java
@@ -0,0 +1,152 @@
+import sa.*;
+import ts.Ts;
+
+public class Sa2ts extends SaDepthFirstVisitor <Void> {
+
+    /* foncName est le nom de la fonction courrante. Si foncName est null, alors on se situe dans le contexte global. Sinon, on se situe
+    dans un contexte local.
+
+       On peut accéder à la table de symbole location d'une fonction à partir de son nom (foncName).
+
+       Lorsqu'une déclaration de variable concerne les paramètres d'une fonction, la valeur de foncParam est true. Sinon, elle est false. */
+
+    Ts table;
+    String foncName;
+    boolean foncParam;
+
+    public Sa2ts(SaNode saRoot) {
+        table = new Ts();
+        foncName = null;
+        foncParam = false;
+        saRoot.accept(this);
+    }
+
+    public Void visit(SaDecVar node){
+        if (foncName == null){
+        // Global context
+            if (table.getVar(node.getNom()) == null){
+                table.addVar(node.getNom(), node.tsItem == null ? 1 : node.tsItem.getTaille());
+            }
+            else {
+                System.out.println("/!\\ ERROR : Variable not declared.");
+                System.exit(0);
+            }
+        }
+
+        else {
+        // Local context or function parameter
+            if (foncParam){
+                if(table.getTableLocale(foncName).getVar(node.getNom()) == null){
+                    table.getTableLocale(foncName).addParam(node.getNom());
+                }
+                else {
+                    System.out.println("/!\\ ERROR : Parameter not declared.");
+                    System.exit(0);
+                }
+            }
+            else {
+                if(table.getTableLocale(foncName).getVar(node.getNom()) == null){
+                    table.getTableLocale(foncName).addVar(node.getNom(), node.tsItem == null ? 1 : node.tsItem.getTaille());
+                }
+                else {
+                    System.out.println("/!\\ ERROR : Variable not declared");
+                    System.exit(0);
+                }
+            }
+        }
+        return null;
+    }
+
+    public Void visit(SaDecTab node){
+        if (foncName == null){
+            if (table.getVar(node.getNom()) == null) table.addVar(node.getNom(), node.getTaille());
+            else {
+                System.out.println("/!\\ ERROR : Variable not declared.");
+                System.exit(0);
+            }
+        }
+        else {
+            System.out.println("/!\\ ERROR : Array must be declared in a global context.");
+            System.exit(0);
+        }
+        return null;
+    }
+
+    public Void visit(SaDecFonc node){
+        Ts newTs = new Ts();
+
+        if (foncName != null){
+            System.out.println("/!\\ ERROR : Function declared in another function.");
+            System.exit(0);
+        }
+
+        if (table.getFct(node.getNom()) != null){
+            System.out.println("/!\\ ERROR : Function not declared.");
+            System.exit(0);
+        }
+        else
+            table.addFct(node.getNom(), node.getParametres() == null ? 0 : node.getParametres().length(), newTs, node);
+
+        foncName = node.getNom();
+
+        if (node.getParametres() != null){
+            foncParam = true;
+            node.getParametres().accept(this);
+        }
+
+        if (node.getVariable() != null){
+            node.getVariable().accept(this);
+        }
+
+        foncParam = false;
+        node.getCorps().accept(this);
+
+        foncName = null;
+
+        return null;
+    }
+
+
+    public Void visit(SaVarSimple node){
+        if (foncName != null && table.getTableLocale(foncName).getVar(node.getNom()) == null && table.getVar(node.getNom()) == null) {
+            System.out.println("/!\\ ERROR : Variable not declared.");
+            System.exit(0);
+        }
+        if (table.getVar(node.getNom()) == null && foncName == null){
+            System.out.println("/!\\ ERROR : Variable not declared.");
+            System.exit(0);
+        }
+        return null;
+    }
+
+    public Void visit(SaVarIndicee node){
+        if (table.getVar(node.getNom()).getTaille() == 1){
+            System.out.println("/!\\ ERROR : Indexed variable.");
+            System.exit(0);
+        }
+        return null;
+    }
+
+    public Void visit(SaAppel node) {
+        if (table.getFct(node.getNom()) == null){
+            System.out.println("/!\\ ERROR : Function not declared.");
+            System.exit(0);
+        }
+
+        if (node.getArguments() != null){
+
+            if (table.getFct(node.getNom()).getNbArgs() < node.getArguments().length()){
+                System.out.println("/!\\ ERROR : Too many arguments.");
+                System.exit(0);
+            }
+            if (table.getFct(node.getNom()).getNbArgs() > node.getArguments().length()){
+                System.out.println("/!\\ ERROR : Not enough arguments.");
+                System.exit(0);
+            }
+        }
+        return null;
+    }
+
+    public Ts getTable() { return table; }
+
+}
